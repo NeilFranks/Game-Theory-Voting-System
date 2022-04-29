f44bdaff19166efa4961b167d3b6611482a8a61f
NeilFranks <neilmichaelfranks@gmail.com>
2022-04-29 14:39:28 -0700

    test four-way tie

--- GT_utils/optimal_mixed_strategy.py
+++ GT_utils/optimal_mixed_strategy.py
@@ -8,35 +8,41 @@ from GT_utils.margin_matrix import construct_margin_matrix_from_preference_matri
 
 
 def calculate_optimal_mixed_strategy_from_margin_matrix(margin_matrix):
-    # use the margin matrix as the payoff matrix for a 2-player zero-sum game
-    game = nash.Game(margin_matrix)
+    # if margin matrix is all zeros, no need to try to compute the optimal mixed strategy;
+    # all candidates have equal probability
+    if margin_matrix.max() == 0 and margin_matrix.min() == 0:
+        optimal_mixed_strategy = [1 / len(margin_matrix)] * len(margin_matrix)
+        
+    else:
+        # use the margin matrix as the payoff matrix for a 2-player zero-sum game
+        game = nash.Game(margin_matrix)
 
-    # use vertex enumeration to compute all optimal mixed strategies
-    optimal_mixed_strategies = game.vertex_enumeration()
+        # use vertex enumeration to compute all optimal mixed strategies
+        optimal_mixed_strategies = game.vertex_enumeration()
 
-    try:
-        optimal_mixed_strategies = list(optimal_mixed_strategies)
+        try:
+            optimal_mixed_strategies = list(optimal_mixed_strategies)
 
-        # each optimal mixed strategy contains a strategy for player 1 and player 2.
-        # we can use either one (they're the same)
-        P1_optimal_mixed_strategies = [
-            optimal_mixed_strategy[0]
-            for optimal_mixed_strategy in optimal_mixed_strategies
-        ]
+            # each optimal mixed strategy contains a strategy for player 1 and player 2.
+            # we can use either one (they're the same)
+            P1_optimal_mixed_strategies = [
+                optimal_mixed_strategy[0]
+                for optimal_mixed_strategy in optimal_mixed_strategies
+            ]
 
-        # if there is more than one optimal mixed strategy,
-        # then the best one is the one the least sum of squares of its values
-        sorted_P1_optimal_mixed_strategies = sorted(
-            P1_optimal_mixed_strategies,
-            key=lambda mixed_strategy: np.sum(mixed_strategy ** 2)
-        )
+            # if there is more than one optimal mixed strategy,
+            # then the best one is the one the least sum of squares of its values
+            sorted_P1_optimal_mixed_strategies = sorted(
+                P1_optimal_mixed_strategies,
+                key=lambda mixed_strategy: np.sum(mixed_strategy ** 2)
+            )
 
-        winner = sorted_P1_optimal_mixed_strategies[0]
+            optimal_mixed_strategy = sorted_P1_optimal_mixed_strategies[0]
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
-        # On any failure, just pick one optimal mixed strategy
-        winner = next(optimal_mixed_strategies)[0]
+            # On any failure, just pick one optimal mixed strategy
+            optimal_mixed_strategy = next(optimal_mixed_strategies)[0]
 
-    return winner
+    return optimal_mixed_strategy
--- README.md
+++ README.md
@@ -17,11 +17,13 @@ The command `./simulate_election.sh` takes arguments:
 - arg1: `path_to_ballot_profile` (default: `"ballot_profiles/example_from_paper.txt"`)
 - arg2: `number_of_simulations` (default: `100`)
 
-The script will simulated an election on a ballot profile `number_of_simulations` times. It is expected that GT voting system will be the only one where the same candidate may not win every simulation. This is due to GT's employment of random selection based on probabilities from the computed optimal mixed strategy.
+The script will simulated an election on a ballot profile `number_of_simulations` times. It is expected that IRV and GT voting systems will be the only ones where the same candidate may not win every simulation. This is due to GT's use of random selection based on probabilities from the computed optimal mixed strategy, and the IRV implementation's use of random selection in the case of ties.
 
 To replicate the simple example from the paper, you can simply run `./simulate_election.sh`
 
 To replicate the Schulze example from the paper, you can run `./simulate_election.sh ballot_profiles/Schulze_test.txt`
+
+To see what happens in the case of a four-way tie, you can run `./simulate_election.sh ballot_profiles/tie.txt`
 
 To see how candidates from the 2020 Iowa democratic caucus might fare under different voting systems, you can run `./simulate_election.sh ballot_profiles/Iowa_caucus.txt`
 
